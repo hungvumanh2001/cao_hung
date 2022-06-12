8b73077d2485682425facc4606df879712d5fd64
hung.vm.dev <hungvumanh2001@gmail.com>
2022-06-12 18:30:01 +0700

    first commit

---
+++ src/main/java/com/example/demo_git/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo_git;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
